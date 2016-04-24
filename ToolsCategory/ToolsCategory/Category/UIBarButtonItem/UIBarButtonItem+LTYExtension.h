6d339eb10bd78787fd5644f8fd99b8304fdac4ba
AYuan <934309587@qq.com>
2016-04-24 13:47:58 +0800

    添加部分工具类

---
+++ ToolsCategory/ToolsCategory/Category/UIBarButtonItem/UIBarButtonItem+LTYExtension.h
@@ -0,0 +1,15 @@
+//
+//  UIBarButtonItem+LTYExtension.h
+//
+//  Created by AYuan on 16/3/5.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBarButtonItem (LTYExtension)
+
++ (UIBarButtonItem *)itemWithImage:(NSString *)image highImage:(NSString *)highImage target:(id)target action:(SEL)action;
+
+
+@end
---
+++ ToolsCategory/ToolsCategory/Category/UIWindow/UIWindow+Launch.h
@@ -0,0 +1,48 @@
+//
+//  UIWindow+Launch.h
+//  Wifi
+//
+//  Created by AYuan on 15/11/19.
+//  Copyright (c) 2015年 ayuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (Launch)
+
+
+/**
+ *  主window
+ */
++(UIWindow *)appWindow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end

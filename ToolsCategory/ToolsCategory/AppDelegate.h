2304207f46389f7293613192df80ced07ad99d8e
AYuan <934309587@qq.com>
2016-04-24 13:22:30 +0800

    setup project

---
+++ ToolsCategory/ToolsCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToolsCategory
+//
+//  Created by AYuan on 16/4/24.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToolsCategory/ToolsCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToolsCategory
+//
+//  Created by AYuan on 16/4/24.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

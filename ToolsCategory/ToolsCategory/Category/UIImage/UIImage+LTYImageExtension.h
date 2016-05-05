b1c9b5033c0f474f787904c6247f41fe6b5aeefa
AYuan <934309587@qq.com>
2016-05-05 08:45:33 +0800

    添加圆角工具类

---
+++ ToolsCategory/ToolsCategory/Category/UIImage/UIImage+LTYImageExtension.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+LTYImageExtension.h
+//
+//  Created by AYuan on 16/5/4.
+//  Copyright © 2016年 AYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (LTYImageExtension)
+
+/**
+ * 生成圆形图片
+ */
+- (UIImage *)circleImage;
+
+@end
